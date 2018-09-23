5e2d5e481d4d91527e33fb38ca748786d0972772
bonoTanya <tatianabonoeva3@gmail.com>
2018-09-23 13:23:24 +0300

    второе задание

--- Second.java
+++ Second.java
@@ -1,4 +1,17 @@
 package homework.git;
 
-public class Second {
+import com.sun.javafx.collections.MappingChange;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Second{
+    public static void main(String[] args) {
+        final Map<String, String> tel = new HashMap<String, String>();
+        tel.put("Андрей", "89120562476");
+        tel.put("Андрей", "82758420847");
+
+        System.out.print(tel.get("Андрей"));
+    }
 }
